35ba9ae5662cc475076a214b080b1da23476e3ee
aline-necchi <alinenecchi@gmail.com>
2022-06-05 18:35:50 -0300

    Aula 06-Tdd

--- app-sorteador-testes/src/componentes/Formulario.test.tsx
+++ app-sorteador-testes/src/componentes/Formulario.test.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { setUncaughtExceptionCaptureCallback } from "process";
+import Formulario from "./Formulario";
 
 test("Quando o input está vazio, novos participantes não podem ser adicionados", () => {
-
-    render(<Formulario/>);
-
-    //encontrar no DOM o input 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
-    //encontrar o botão 
-    const botao = screen.getByRole('button');
-    //garantir que o input esteja no ducumento
-    expect(input).toBeInTheDocument();
-    //garantir que o botão esteja desabilitado
-    setUncaughtExceptionCaptureCallback(botao).toBeDisabled();
+  render(<Formulario />);
+  //encontrar no DOM o input
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  //encontrar o botão
+  const botao = screen.getByRole("button");
+  //garantir que o input esteja no ducumento
+  expect(input).toBeInTheDocument();
+  //garantir que o botão esteja desabilitado
+  expect(botao).toBeDisabled();
 });
